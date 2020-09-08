52db4c469ca1887cb15aa2cd8bd846fa1b5bb6e7
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-08 14:50:30 +0530

    getter and setter methods for appetizers, mains and desserts

--- app.js
+++ app.js
@@ -4,4 +4,22 @@ const menu = {
     mains: [],
     desserts: [],
   },
+  get appetizers() {
+    return this.courses.appetizers;
+  },
+  get mains() {
+    return this.courses.mains;
+  },
+  get desserts() {
+    return this.courses.desserts;
+  },
+  set appetizers(appetizers) {
+    this._courses.appetizers = apptizers;
+  },
+  set mains(mains) {
+    this._courses.mains = mains;
+  },
+  set desserts(desserts) {
+    this._courses.desserts = desserts;
+  },
 };
