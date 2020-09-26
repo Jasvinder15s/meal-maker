b3fac926a89759dfc6ac35aecc655ad0ab75ef51
Jasvinder Singh <jvsinghk@gmail.com>
2020-09-26 14:37:57 +0530

    completed meal maker

--- app.js
+++ app.js
@@ -7,60 +7,76 @@ const menu = {
   get appetizers() {
     return this._courses.appetizers;
   },
+  set appetizers(meal) {
+    if (typeof meal === "object") {
+      //checks that the input is of the correct data type
+      this._courses.appetizers.push(meal);
+    } else {
+      console.log("Please input meal as object");
+    }
+  },
   get mains() {
     return this._courses.mains;
+  },
+  set mains(meal) {
+    if (typeof meal === "object") {
+      //checks that the input is of the correct data type
+      this._courses.mains.push(meal);
+    } else {
+      console.log("Please input meal as object");
+    }
   },
   get desserts() {
     return this._courses.desserts;
   },
-  set appetizers(appetizers) {
-    this._courses.appetizers = apptizers;
-  },
-  set mains(mains) {
-    this._courses.mains = mains;
-  },
-  set desserts(desserts) {
-    this._courses.desserts = desserts;
+  set desserts(meal) {
+    if (typeof meal === "object") {
+      //checks that the input is of the correct data type
+      this._courses.desserts.push(meal);
+    } else {
+      console.log("Please input meal as object");
+    }
   },
   get courses() {
-    return {
-      appetizers: this.appetizers,
-      mains: this.mains,
-      desserts: this.desserts,
-    };
+    return this._courses;
   },
   addDishToCourse(courseName, dishName, dishPrice) {
-    const dish = {
-      name: dishName,
-      price: dishPrice,
-    };
-    return this._courses[courseName].push(dish);
+    if (Object.keys(this._courses).includes(courseName)) {
+      //checks to make sure the input courseName matches one of the three course names defined within the _courses object.
+      const dish = {
+        name: dishName,
+        price: dishPrice,
+      };
+      this[courseName] = dish;
+    } else {
+      console.log("Please Enter a valid course name");
+    }
   },
-  getRandomDishFromCourse(courseName) {
-      const dishes = this._courses[courseName];
-      const randomIndex = Math.floor(Math.random * dishes.length);
-      return dishes[randomIndex];
+  getRandomDishFromCourseName(courseName) {
+    let dishes = this[courseName];
+    let randomIndex = Math.floor(Math.random() * dishes.length);
+    return dishes[randomIndex];
   },
   generateRandomMeal() {
-      const appetizer = this.getRandomDishFromCourse('appetizers');
-      const main = this.getRandomDishFromCourse('mains');
-      const dessert = this.getRandomDishFromCourse('desserts');
-      const totalPrice = appetizer.price + main.price + dessert.price;
-      return `Your meal is ${appetizer.name}, ${main.name}, 
-      and ${dessert.name}, and the total price is ${totalPrice}`
-  }
+    const appetizer = this.getRandomDishFromCourseName("appetizers");
+    const main = this.getRandomDishFromCourseName("mains");
+    const dessert = this.getRandomDishFromCourseName("desserts");
+    const totalPrice = appetizer.price + main.price + dessert.price;
+    return `Your meal is ${appetizer.name}, ${main.name}, 
+      and ${dessert.name}, and the total price is ${totalPrice}`;
+  },
 };
 
-menu.addDishToCourse('appetizers', 'salad', 4.00);
-menu.addDishToCourse('appetizers', 'wings', 4.50);
-menu.addDishToCourse('appetizers', 'fries', 5.00);
+menu.addDishToCourse("appetizers", "salad", 4.0);
+menu.addDishToCourse("appetizers", "wings", 4.5);
+menu.addDishToCourse("appetizers", "fries", 5.0);
 
 menu.addDishToCourse("mains", "steak", 10.25);
 menu.addDishToCourse("mains", "salmon", 7.75);
-menu.addDishToCourse("mains", "tofu", 11.20);
+menu.addDishToCourse("mains", "tofu", 11.2);
 
 menu.addDishToCourse("desserts", "ice cream", 3.0);
-menu.addDishToCourse("desserts", "coffe", 3.00);
+menu.addDishToCourse("desserts", "coffe", 3.0);
 menu.addDishToCourse("desserts", "cake", 3.25);
 
 const meal = menu.generateRandomMeal();
